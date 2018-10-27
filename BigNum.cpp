84e3b801d971dd318558c1ba10f8bf92da82911d
kiwiyou | C6H6 <kiwiyou.dev@gmail.com>
2018-10-27 19:10:09 +0900

    Copyright 수정

--- BigNum.cpp
+++ BigNum.cpp
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) 2018 kmc7468
+ * Copyright (c) 2018 kmc7468, kiwiyou
  *
  * Permission is hereby granted, reset of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
